965951596b409159d3cdd3a7fe9122894f7cac47
aashpv <yungtommi@gmail.com>
2024-08-27 15:08:47 +0400

    [add] comments

--- 02_concurrency_sqrt/main.go
+++ 02_concurrency_sqrt/main.go
@@ -10,59 +10,40 @@ import (
 взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
 */
 
-func unbufferedChannel(nums []int) {
-	ch := make(chan int)
-
-	for _, num := range nums {
-		go func(num int) {
-			ch <- num * num
-		}(num)
-	}
-
-	for range nums {
-		fmt.Println(<-ch)
-	}
-
-	close(ch)
-}
-
-func bufferedChannel(nums []int) {
-	ch := make(chan int, len(nums))
-
-	for _, num := range nums {
-		go func(num int) {
-			ch <- num * num
-		}(num)
-	}
-
-	for range nums {
-		fmt.Println(<-ch)
-	}
-
-	close(ch)
+func square(num int, wg *sync.WaitGroup) {
+	defer wg.Done() // Уменьшение счетчика WaitGroup
+	fmt.Println(num * num)
 }
 
 func waitGroup(nums []int) {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // Создание экземпляра WaitGroup
 
 	for _, num := range nums {
-		wg.Add(1)
-
-		go func(num int) {
-			defer wg.Done()
-			sq := num * num
-			fmt.Println(sq)
-		}(num)
-
-		wg.Wait()
+		wg.Add(1)           // Увеличение счетчика WaitGroup
+		go square(num, &wg) // Запуск горутины для вычисления квадрата
 	}
+	wg.Wait() // Ожидание завершения всех горутин
+}
+
+func bufferedChannel(nums []int) {
+	ch := make(chan int, len(nums)) // Создание буферизированного канала
+
+	for _, num := range nums {
+		go func(num int) {
+			ch <- num * num // Отправка квадрата числа в канал
+		}(num)
+	}
+
+	for range nums {
+		fmt.Println(<-ch) // Получение значения из канала и вывод его на экран
+	}
+	close(ch) // Закрытие канала
 }
 
 func main() {
 	nums := []int{2, 4, 6, 8, 10}
-	unbufferedChannel(nums)
-	fmt.Println("-------------------")
+
 	bufferedChannel(nums)
-	fmt.Println("--------------------")
+	fmt.Println("-------------------")
 	waitGroup(nums)
 }
