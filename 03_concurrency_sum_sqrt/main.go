104646232364c4a2781ba248977e2378faa9f613
aashpv <yungtommi@gmail.com>
2024-08-27 15:16:57 +0400

    [add] comments

--- 03_concurrency_sum_sqrt/main.go
+++ 03_concurrency_sum_sqrt/main.go
@@ -7,69 +7,53 @@ import (
 
 /*
 Дана последовательность чисел: 2,4,6,8,10.
-Найти сумму их квадратов(2^2+3^2+4^2….) с использованием конкурентных вычислений.
+Найти сумму их квадратов (2^2+3^2+4^2….) с использованием конкурентных вычислений.
 */
-
-func unbufferedChannel(nums []int) int {
-	sum := 0
-	ch := make(chan int)
-
-	for _, num := range nums {
-		go func(num int) {
-			ch <- num * num
-		}(num)
-	}
-
-	for range nums {
-		sum += <-ch
-	}
-	close(ch)
-
-	return sum
-}
 
 func bufferedChannel(nums []int) int {
 	sum := 0
-	ch := make(chan int, len(nums))
+	ch := make(chan int, len(nums)) // Создание буферизированного канала
 
 	for _, num := range nums {
 		go func(num int) {
-			ch <- num * num
+			ch <- num * num // Отправка квадрата числа в канал
 		}(num)
 	}
 
 	for range nums {
-		sum += <-ch
+		sum += <-ch // Получение значений из канала и суммирование их
 	}
-	close(ch)
+	close(ch) // Закрытие канала
 
-	return sum
+	return sum // Возврат общей суммы
 }
 
 func waitGroup(nums []int) int {
 	sum := 0
-	var wg sync.WaitGroup
+	var mu sync.Mutex     // Создание мьютекса для синхронизации доступа к переменной
+	var wg sync.WaitGroup // Создание экземпляра WaitGroup
 
 	for _, num := range nums {
-		wg.Add(1)
+		wg.Add(1) // Увеличение счетчика WaitGroup
 
 		go func(num int) {
-			defer wg.Done()
-			sq := num * num
+			defer wg.Done() // Уменьшение счетчика WaitGroup
+			sq := num * num // Вычисление квадрата числа
+			mu.Lock()       // Захват мьютекса перед изменением переменной
 			sum += sq
+			mu.Unlock() // Освобождение мьютекса
 		}(num)
-
-		wg.Wait()
 	}
 
-	return sum
+	wg.Wait() // Ожидание завершения всех горутин
+
+	return sum // Возврат общей суммы
 }
 
 func main() {
 	nums := []int{2, 4, 6, 8, 10}
-	fmt.Println(unbufferedChannel(nums))
-	fmt.Println("-------------------")
+
 	fmt.Println(bufferedChannel(nums))
-	fmt.Println("--------------------")
+	fmt.Println("-------------------")
 	fmt.Println(waitGroup(nums))
 }
